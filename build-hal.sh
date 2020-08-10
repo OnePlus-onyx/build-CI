8d51f6ffd4704d757d9f7d42dc109bad4a1bd3bd
ittat <ittat@live.com>
2020-08-10 19:48:15 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,7 +5,6 @@ export ANDROID_ROOT=~/work/halium
 export PATH=$PATH:~/bin/
 #patch
 cd $ANDROID_ROOT
-./hybris-patches/apply-patches.sh --mb
 # replace something
 #sed -i 's/external\/selinux/external\/selinux external\/libcurl/g' build/core/main.mk
 export LC_ALL=C
