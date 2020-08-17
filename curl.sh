309a400c311e4b93fab5162a22a2064a4a3c6af1
ittat <ittat@live.com>
2020-08-17 15:29:21 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,6 +1,6 @@
-filename="B2G.tar.zst1"
+filename="B2G.tar.zst"
 
-file_id="1WO40RS1dkMgncexmJg81aMZsVv40UKx2"
+file_id="10cIH4SOP8QO1HthyK2MToFHP1u5T9hM"
 
 query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
 | perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
