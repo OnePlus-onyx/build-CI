900b78b1e86d9851cb6f6a587067cd7db67c551d
ittat <ittat@live.com>
2020-08-17 13:45:45 +0800

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,11 @@
+filename=""
+
+file_id=""
+
+query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
+| perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
+| sed -e 's/amp;//g' | sed -n 2p`
+
+url="https://drive.google.com$query"
+
+curl -b ./cookie.txt -L -o ${filename} $url
