ae1d7f56e6cf31fc9dc6c9126f635257ed72c215
ittat <ittat@live.com>
2020-09-15 13:30:36 +0800

    Create chroot.sh

---
+++ chroot.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+sudo mount --rbind /dev rootfs/dev
+sudo mount --rbind /proc rootfs/proc
+sudo mount --rbind /sys rootfs/sys
+sudo mount -t tmpfs tmpfs rootfs/tmp
+sudo mount -vt devpts devpts rootfs/dev/pts -o gid=5,mode=620
+sudo mount -vt tmpfs tmpfs rootfs/run
+chroot ./rootfs
