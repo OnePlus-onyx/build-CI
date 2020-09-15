9cf07c8d44659377a77291c65d3593a348dcef7e
ittat <ittat@live.com>
2020-09-15 13:31:40 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -6,4 +6,4 @@ sudo mount --rbind /sys rootfs/sys
 sudo mount -t tmpfs tmpfs rootfs/tmp
 sudo mount -vt devpts devpts rootfs/dev/pts -o gid=5,mode=620
 sudo mount -vt tmpfs tmpfs rootfs/run
-chroot ./rootfs
+#chroot ./rootfs
