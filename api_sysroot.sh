fe95ac598e5586f95e17bc7795d3265095c4b45f
ittat <ittat@live.com>
2020-10-26 23:04:35 +0800

    Update api_sysroot.sh

--- api_sysroot.sh
+++ api_sysroot.sh
@@ -3,6 +3,53 @@
 export GONK_PATH=`pwd`
 export GECKO_PATH=${GONK_PATH}/gecko
 
+# Prepare the device-specific paths in the AOSP build files
+case "${TARGET_ARCH}" in
+    arm)
+        ARCH_NAME="arm"
+        ARCH_ABI="androideabi"
+        ;;
+    arm64)
+        ARCH_NAME="aarch64"
+        ARCH_ABI="android"
+        TARGET_TRIPLE=$ARCH_NAME-linux-$ARCH_ABI
+        BINSUFFIX=64
+        ;;
+    x86)
+        ARCH_NAME="i686"
+        ARCH_ABI="android"
+        TARGET_TRIPLE=$ARCH_NAME-linux-$ARCH_ABI
+        ;;
+    x86_64)
+        ARCH_NAME="x86"
+        ARCH_ABI="android"
+        BINSUFFIX=64
+        ;;
+    *)
+        echo "Unsupported $TARGET_ARCH"
+        exit 1
+        ;;
+esac
+
+TARGET_TRIPLE=${TARGET_TRIPLE:-$TARGET_ARCH-linux-$ARCH_ABI}
+
+if [ "$TARGET_ARCH_VARIANT" = "$TARGET_ARCH" ] ||
+   [ "$TARGET_ARCH_VARIANT" = "generic" ]; then
+TARGET_ARCH_VARIANT=""
+else
+TARGET_ARCH_VARIANT="_$TARGET_ARCH_VARIANT"
+fi
+
+if [ "$TARGET_CPU_VARIANT" = "$TARGET_ARCH" ] ||
+   [ "$TARGET_CPU_VARIANT" = "generic" ]; then
+TARGET_CPU_VARIANT=""
+else
+TARGET_CPU_VARIANT="_$TARGET_CPU_VARIANT"
+fi
+
+ARCH_FOLDER="${TARGET_ARCH}${TARGET_ARCH_VARIANT}${TARGET_CPU_VARIANT}"
+
+
 # Package the sysroot
 SYSROOT_PREBUILTS="prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/lib/gcc/x86_64-linux-android/4.9.x"
 
